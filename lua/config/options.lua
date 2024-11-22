8efb745cd0972c72f10f8dbdb359d31c16612532
Tommaso Clini <tommasoclini@gmail.com>
2024-11-22 15:38:02 +0100

    first commit, clangd not working well

---
+++ lua/config/options.lua
@@ -0,0 +1,27 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.autoformat = false
+--[[vim.g.rustaceanvim = {
+  -- Plugin configuration
+  tools = {
+  },
+  -- LSP configuration
+  server = {
+    on_attach = function(client, bufnr)
+      -- you can also put keymaps in here
+    end,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ['rust-analyzer'] = {
+        cargo = {
+          allTargets = false
+        }
+      },
+    },
+  },
+  -- DAP configuration
+  dap = {
+  },
+}
+--]]
