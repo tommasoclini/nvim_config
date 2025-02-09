b95ffbb08d4bf87d924868b8d1f992cbb2c7f984
Tommaso Clini <tommasoclini@gmail.com>
2025-02-09 12:34:57 +0100

    remove clangd conf for esp idf, vscode has unfortunately won

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -3,32 +3,6 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "projekt0n/github-nvim-theme" },
   {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      local clangd
-      local esp_idf_path = os.getenv("IDF_PATH")
-      if esp_idf_path then
-        clangd = {
-          cmd = {
-            "clangd",
-            "--query-driver=/home/tommaso/.espressif/tools/riscv32-esp-elf/esp-13.2.0_20240530/riscv32-esp-elf/bin/*,"
-              .. "/home/tommaso/.espressif/tools/xtensa-esp-elf/esp-13.2.0_20240530/xtensa-esp-elf/bin/*",
-          },
-        }
-      else
-        clangd = {
-          cmd = {
-            "clangd-19",
-            "--query-driver=/usr/bin/arm-none-eabi-*,/usr/bin/gcc-*,/usr/bin/avr*",
-          },
-        }
-      end
-      opts.servers = vim.tbl_extend("force", opts.servers or {}, {
-        clangd = clangd,
-      })
-    end,
-  },
-  {
     "christoomey/vim-tmux-navigator",
     cmd = {
       "TmuxNavigateLeft",
