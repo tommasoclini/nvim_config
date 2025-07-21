f8bd7f0b70d04bfee73f0284976677fc34cf14b3
Tommaso Clini <tommasoclini@gmail.com>
2025-07-21 12:03:02 +0200

    clangd, rustowl and image shit

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -2,7 +2,8 @@ return {
   { "olimorris/onedarkpro.nvim", lazy = false },
   { "ellisonleao/gruvbox.nvim" },
   { "projekt0n/github-nvim-theme" },
-  --[[{
+  ---[[
+  {
     "christoomey/vim-tmux-navigator",
     cmd = {
       "TmuxNavigateLeft",
@@ -18,11 +19,53 @@ return {
       { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
-  },]]
+  }, --]]
+  {
+    "cordx56/rustowl",
+    version = "*", -- Latest stable version
+    build = "cargo binstall rustowl",
+    lazy = false, -- This plugin is already lazy
+    opts = {
+      client = {
+        on_attach = function(_, buffer)
+          vim.keymap.set("n", "<leader>o", function()
+            require("rustowl").toggle(buffer)
+          end, { buffer = buffer, desc = "Toggle RustOwl" })
+        end,
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local clangd
+      --local esp_idf_path = os.getenv("IDF_PATH")
+      --[[if esp_idf_path then
+        clangd = {
+          cmd = {
+            "clangd",
+            "--query-driver=/home/tommaso/.espressif/tools/riscv32-esp-elf/esp-13.2.0_20240530/riscv32-esp-elf/bin/*,"
+              .. "/home/tommaso/.espressif/tools/xtensa-esp-elf/esp-13.2.0_20240530/xtensa-esp-elf/bin/*",
+          },
+        }
+      else]]
+      clangd = {
+        cmd = {
+          "clangd-19",
+          "--query-driver=/usr/bin/arm-none-eabi-*,/usr/bin/gcc-*",
+        },
+      }
+      --end
+      opts.servers = vim.tbl_extend("force", opts.servers or {}, {
+        clangd = clangd,
+      })
+    end,
+  },
   {
     "folke/snacks.nvim",
     opts = {
       -- scroll = { enabled = false },
+      image = { enabled = true },
       picker = {
         sources = {
           explorer = {
