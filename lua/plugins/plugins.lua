31ed2ac330eb2ad074c8909644fcd1a50cb49c2a
Tommaso Clini <tommasoclini@gmail.com>
2025-08-01 09:03:21 +0200

    rustowl is not needed, and we need fucking git shit

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -21,21 +21,6 @@ return {
     },
   }, --]]
   {
-    "cordx56/rustowl",
-    version = "*", -- Latest stable version
-    build = "cargo binstall rustowl",
-    lazy = false, -- This plugin is already lazy
-    opts = {
-      client = {
-        on_attach = function(_, buffer)
-          vim.keymap.set("n", "<leader>o", function()
-            require("rustowl").toggle(buffer)
-          end, { buffer = buffer, desc = "Toggle RustOwl" })
-        end,
-      },
-    },
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
       local clangd
