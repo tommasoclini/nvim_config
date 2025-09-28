b847e2e7c9e931818fb2314f6125a9f28696abea
Tommaso Clini <tommasoclini@gmail.com>
2025-09-28 13:51:18 +0200

    disable wrap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
-vim.o.wrap = true
+vim.o.wrap = false
 -- vim.wo.signcolumn = "no"
 -- vim.cmd("set spell")
